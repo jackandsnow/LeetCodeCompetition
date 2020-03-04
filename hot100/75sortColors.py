5be12bdc0414085932ac98f2fdb300ec832d00b4
jackandsnow <jyjlwx@outlook.com>
2020-03-04 23:12:18 +0800

    add leetcode hot No.75

---
+++ hot100/75sortColors.py
@@ -0,0 +1,53 @@
+"""
+75. 颜色分类
+给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+
+此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+
+注意:
+不能使用代码库中的排序函数来解决这道题。
+
+示例:
+
+    输入: [2,0,2,1,1,0]
+    输出: [0,0,1,1,2,2]
+
+进阶：
+
+    一个直观的解决方案是使用计数排序的两趟扫描算法。
+    首先，迭代计算出0、1 和 2 元素的个数，然后按照0、1、2的排序，重写当前数组。
+    你能想出一个仅使用常数空间的一趟扫描算法吗？
+
+"""
+
+
+def quick_sort(nums, l, r):
+    if r <= l:
+        return
+    mid = (l + r) // 2
+    key = nums[mid]
+    nums[mid], nums[r] = nums[r], nums[mid]
+    tl, tr = l, r
+    while tl < tr:
+        while tl < tr and nums[tl] <= key:
+            tl += 1
+        while tl < tr and key <= nums[tr]:
+            tr -= 1
+        nums[tl], nums[tr] = nums[tr], nums[tl]
+    nums[tl], nums[r] = nums[r], nums[tl]
+    quick_sort(nums, l, tl - 1)
+    quick_sort(nums, tl + 1, r)
+
+
+def sortColors(nums):
+    """
+    :type nums: List[int]
+    :rtype: None Do not return anything, modify nums in-place instead.
+    """
+    print(nums)
+    quick_sort(nums, 0, len(nums) - 1)
+    print(nums)
+
+
+if __name__ == '__main__':
+    sortColors([2, 0, 2, 1, 1, 3, 0])
